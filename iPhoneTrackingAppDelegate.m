f8a3d492ad6fac09ced7369cdcf2d8865ec95008
Tom MacWright <macwright@gmail.com>
2011-04-20 15:55:13 -0700

    Log actual filename in Console too

--- iPhoneTrackingAppDelegate.m
+++ iPhoneTrackingAppDelegate.m
@@ -116,6 +116,8 @@
       if (dbFileName==nil) {
         NSLog(@"No consolidated.db file found in '%@'", newestFolder);
         continue;
+      } else {
+          NSLog(@"File found at '%@'", dbFileName);
       }
 
       NSString* dbFilePath = [newestFolder stringByAppendingPathComponent:dbFileName];
