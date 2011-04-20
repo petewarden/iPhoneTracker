535e99dcbf93695184e9abc7b0f82689eb370e9c
Steven Robbins <ste.robbins@gmail.com>
2011-04-20 17:53:04 +0100

    Support for 10.5

--- iPhoneTrackingAppDelegate.h
+++ iPhoneTrackingAppDelegate.h
@@ -8,7 +8,11 @@
 #import <Cocoa/Cocoa.h>
 #import <WebKit/WebKit.h>
 
+#if (MAC_OS_X_VERSION_MAX_ALLOWED <= MAC_OS_X_VERSION_10_5)
+@interface iPhoneTrackingAppDelegate : NSObject {
+#else
 @interface iPhoneTrackingAppDelegate : NSObject <NSApplicationDelegate> {
+#endif
   NSWindow *window;
   WebView *webView;
   WebScriptObject* scriptObject;
