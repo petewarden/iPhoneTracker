79e6dbb73e6e676da86b43d8b56f89082c5c45c1
Pete Warden <pete@petewarden.com>
2011-04-23 17:49:38 -0700

    Added credit to Peter Plavchan

--- iPhoneTrackingAppDelegate.h
+++ iPhoneTrackingAppDelegate.h
@@ -30,6 +30,7 @@
 #import <WebKit/WebKit.h>
 
 // See http://stackoverflow.com/questions/1496788/building-for-10-5-in-xcode-3-2-on-snow-leopard-error
+// Thanks to Peter Plavchan who also suggested this
 #if (MAC_OS_X_VERSION_MAX_ALLOWED <= MAC_OS_X_VERSION_10_5)
 @interface iPhoneTrackingAppDelegate : NSObject
 #else
